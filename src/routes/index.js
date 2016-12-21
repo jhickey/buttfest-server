55bcf0d0c6f196096069d889471fcab81fde620e
jhickey <jameshickey@me.com>
2016-12-21 16:30:08 -0500

    Set temperature type

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,24 +3,25 @@ import StatsD from 'node-statsd';
 import sio from '../socket';
 
 const statsdClient = new StatsD();
-
 const router = express.Router();
+
+const DEFAULT_TEMP_TYPE = 'ambient';
 
 router.get('/', (req, res) => {
     res.json({ok: true});
 });
 
 router.post('/',  (req, res) => {
-    const {temperature} = req.body;
+    const {temperature, type = DEFAULT_TEMP_TYPE} = req.body;
     if (!temperature) {
         res.status(400);
         res.json({error: 'Invalid request'});
         return;
     }
     global.temperature = temperature;
-    res.json({temperature});
-    sio.io().emit('temperature', {temperature});
-    statsdClient.gauge('grill_temp', temperature);
+    res.json({temperature, type});
+    sio.io().emit('temperature', {temperature, type});
+    statsdClient.gauge('grill_temp', temperature, [`type:${type}`]);
 });
 
 export default router;
